4939c222dca1668bb664b5ab034fa0ac5ff7f7d6
Lauren DeLago <123128869+lcarinad@users.noreply.github.com>
2023-11-03 15:29:19 -0400

    pet model

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, request, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
-
+from models import db, connect_db, Pet
 app = Flask(__name__)
 app.config['SECRET_KEY']='abc123'
+app.config["SQLALCHEMY_DATABASE_URL"] = "postgresql:///pet_adoption_db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
 app.debug = True
 
 toolbar=DebugToolbarExtension(app)
