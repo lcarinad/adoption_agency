95e09e03520db88bf7524932c4e88e46e5739fba
Lauren DeLago <123128869+lcarinad@users.noreply.github.com>
2023-11-04 15:40:52 -0400

    styling

--- seed.py
+++ seed.py
@@ -9,8 +9,8 @@ fluffy = Pet(name='Fluffy', species='Dog', photo_url='https://media-be.chewy.com
              age=3, notes='Friendly and playful', available=False)
 whiskers = Pet(name='Whiskers', species='Cat', photo_url='https://cdn.britannica.com/39/7139-050-A88818BB/Himalayan-chocolate-point.jpg',
                age=2, notes='Loves to cuddle', available=True)
-bubbles = Pet(name='Bubbles', species='Fish', photo_url='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzp8bihi179-GmniXboTsMBGvLXbRFlTUUD_HJq8d7cN6B2Z4TpKGSvkzkSvo7-k8X1qM&usqp=CAU',
-               age=1, notes='Beautiful colors', available=True)
+bubbles = Pet(name='Hammy', species='Porcupine', photo_url='https://media.istockphoto.com/id/119869206/photo/four-toed-hedgehog-atelerix-albiventris-3-weeks-old-white-background.jpg?s=612x612&w=0&k=20&c=HegzlayFTAZ2gJ0dg85SxGxm3-21LxeIAjh17mALgAw=',
+               age=None, notes=None, available=True)
 
 db.session.add_all([fluffy, whiskers, bubbles])
 db.session.commit()
